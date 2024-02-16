522edf14c90906d79aecc12ec4ec7d4c84a6c99c
krasi <krasimir.xristov@gmail.com>
2024-02-16 15:20:38 +0100

    Create some dummy routes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,24 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+import SignUp from './pages/SignUp';
+import Dashboard from './pages/Dashboard';
+import Projects from './pages/Projects';
+import Signin from './pages/SignIn';
+
 const App = () => {
-  return <h1 className='flex flex-1'>Hello world!</h1>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/sign-up' element={<SignUp />} />
+        <Route path='/sign-in' element={<Signin />} />
+        <Route path='/dashboard' element={<Dashboard />} />
+        <Route path='/projects' element={<Projects />} />
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default App;
