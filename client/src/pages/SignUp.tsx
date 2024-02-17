ba47f6c224a54d3279574fbd790b4039c8dada51
krasi <krasimir.xristov@gmail.com>
2024-02-17 01:22:03 +0100

    add functionality to the sign up page

--- client/src/pages/SignUp.tsx
+++ client/src/pages/SignUp.tsx
@@ -1,7 +1,50 @@
-import { Button, Label, TextInput } from 'flowbite-react';
-import { Link } from 'react-router-dom';
+import { Alert, Button, Label, Spinner, TextInput } from 'flowbite-react';
+import React, { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import { FormDataType } from '../types';
 
 const SignUp = () => {
+  const navigate = useNavigate();
+  const [formData, setFormData] = useState({} as FormDataType);
+  const [errorMessage, setErrorMessage] = useState('');
+  const [loading, setLoading] = useState(false);
+
+  const handleChange = (e: React.ChangeEvent) => {
+    setFormData({ ...formData, [e.target.id]: e.target.value.trim() });
+  };
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+
+    if (!formData.username || !formData.email || !formData.password) {
+      return setErrorMessage('Please fill out all fields');
+    }
+    try {
+      setLoading(true);
+      setErrorMessage('');
+      const res = await fetch('/api/auth/signup', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+
+      if (!res.ok) {
+        setLoading(false);
+        return setErrorMessage(data.message);
+      }
+
+      if (res.ok) {
+        setLoading(false);
+        navigate('/');
+      }
+    } catch (error: { message: string }) {
+      setErrorMessage(error.message);
+      setLoading(false);
+    }
+  };
   return (
     <div className='min-h-screen mt-20'>
       <div className='flex p-3 max-w-3xl mx-auto flex-col md:flex-row md:items-center gap-5'>
@@ -18,25 +61,46 @@ const SignUp = () => {
           </p>
         </div>
         <div className='flex-1'>
-          <form className='flex flex-col gap-4'>
+          <form className='flex flex-col gap-4' onSubmit={handleSubmit}>
             <div>
               <Label value='Your username' />
-              <TextInput type='text' placeholder='Username' id='username' />
+              <TextInput
+                type='text'
+                placeholder='Username'
+                id='username'
+                onChange={handleChange}
+              />
             </div>
             <div>
               <Label value='Your email' />
               <TextInput
-                type='text'
+                type='email'
                 placeholder='Name@company.com'
                 id='email'
+                onChange={handleChange}
               />
             </div>
             <div>
               <Label value='Your password' />
-              <TextInput type='text' placeholder='Password' id='password' />
+              <TextInput
+                type='password'
+                placeholder='Password'
+                id='password'
+                onChange={handleChange}
+              />
             </div>
-            <Button gradientDuoTone='greenToBlue' type='submit'>
-              Sign Up
+            <Button
+              gradientDuoTone='greenToBlue'
+              type='submit'
+              disabled={loading}
+            >
+              {loading ? (
+                <div>
+                  <Spinner size='sm' /> <span className='pl-3'>Loading...</span>{' '}
+                </div>
+              ) : (
+                'Sign up'
+              )}
             </Button>
           </form>
           <div className='flex gap-2 text-sm mt-5'>
@@ -45,6 +109,11 @@ const SignUp = () => {
               Sign in here.
             </Link>
           </div>
+          {errorMessage && (
+            <Alert className='mt-5' color='failure'>
+              {errorMessage}
+            </Alert>
+          )}
         </div>
       </div>
     </div>
---
+++ client/src/types.ts
@@ -0,0 +1,5 @@
+export type FormDataType = {
+  username: string;
+  email: string;
+  password: string;
+};
