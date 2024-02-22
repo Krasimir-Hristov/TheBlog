f1e3283415db51a6351044bb1e07bf55106d2319
krasi <krasimir.xristov@gmail.com>
2024-02-22 22:55:50 +0100

    Update the header

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,10 +1,26 @@
-import { Button, Navbar, NavbarCollapse, TextInput } from 'flowbite-react';
-import { AiOutlineSearch } from 'react-icons/ai';
-import { FaMoon } from 'react-icons/fa';
+import {
+  Avatar,
+  Button,
+  Dropdown,
+  Navbar,
+  NavbarCollapse,
+  TextInput,
+} from 'flowbite-react';
+
 import { Link, useLocation } from 'react-router-dom';
+
+import { useSelector } from 'react-redux';
+
+import { FaMoon } from 'react-icons/fa';
+import { AiOutlineSearch } from 'react-icons/ai';
+import { RootState } from '../redux/store';
 
 const Header = () => {
   const path = useLocation().pathname;
+
+  const { currentUser } = useSelector((state: RootState) => state.user);
+
+  console.log(currentUser);
 
   return (
     <Navbar className='border-b-2'>
@@ -33,11 +49,33 @@ const Header = () => {
         <Button className='w-12 h-10 hidden sm:inline' color='gray' pill>
           <FaMoon />
         </Button>
-        <Link to='/sign-in'>
-          <Button gradientDuoTone='greenToBlue' outline>
-            Sign in
-          </Button>
-        </Link>
+        {currentUser ? (
+          <Dropdown
+            arrowIcon={false}
+            inline
+            label={
+              <Avatar alt='user' img={currentUser?.profilePicture} rounded />
+            }
+          >
+            <Dropdown.Header>
+              <span className='block text-sm'>@{currentUser.username}</span>
+              <span className='block text-sm font-medium truncate'>
+                {currentUser.email}
+              </span>
+            </Dropdown.Header>
+            <Link to={'/dashboard?tab=profile'}>
+              <Dropdown.Item>Profile</Dropdown.Item>
+            </Link>
+            <Dropdown.Divider />
+            <Dropdown.Item>Sign out</Dropdown.Item>
+          </Dropdown>
+        ) : (
+          <Link to='/sign-in'>
+            <Button gradientDuoTone='greenToBlue' outline>
+              Sign in
+            </Button>
+          </Link>
+        )}
         <Navbar.Toggle />
       </div>
       <NavbarCollapse>
--- client/src/redux/store.ts
+++ client/src/redux/store.ts
@@ -25,5 +25,5 @@ export const store = configureStore({
 export const persistor = persistStore(store);
 
 export type RootState = ReturnType<typeof store.getState>;
-// Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
+
 export type AppDispatch = typeof store.dispatch;
