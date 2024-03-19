ec7c24de7ca880ad33e6caf76731e1dd3945d884
krasi <krasimir.xristov@gmail.com>
2024-03-19 18:56:03 +0100

    add posts section

--- client/src/pages/Dashboard.tsx
+++ client/src/pages/Dashboard.tsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from 'react';
 import { useLocation } from 'react-router-dom';
 import DashSidebar from '../components/DashSidebar';
 import DashProfile from '../components/DashProfile';
+import DashPosts from '../components/DashPosts';
 
 const Dashboard = () => {
   const location = useLocation();
@@ -18,7 +19,9 @@ const Dashboard = () => {
   return (
     <div className='min-h-screen flex flex-col md:flex-row'>
       <div className='md:w-56'>{<DashSidebar />}</div>
+
       {tab === 'profile' && <DashProfile />}
+      {tab === 'posts' && <DashPosts />}
     </div>
   );
 };
