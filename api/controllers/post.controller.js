47ca73c10c0880f6837e9d142674880090f10c88
krasi <krasimir.xristov@gmail.com>
2024-03-22 12:43:41 +0100

    Create get user API route

--- api/controllers/post.controller.js
+++ api/controllers/post.controller.js
@@ -85,9 +85,12 @@ export const deletePost = async (req, res, next) => {
 };
 
 export const updatePost = async (req, res, next) => {
+
+
   if (!req.user.isAdmin || req.user.id !== req.params.userId) {
     return next(errorHandler(403, 'You are not allowed to update this post'));
-  }
+  };
+
   try {
     const updatedPost = await Post.findByIdAndUpdate(
       req.params.postId,
