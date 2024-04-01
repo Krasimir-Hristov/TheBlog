b7d7e2abae541e06fb0859731d2df6aa4cd949c4
krasi <krasimir.xristov@gmail.com>
2024-04-01 17:10:04 +0200

    Complete the home page

--- client/src/types.ts
+++ client/src/types.ts
@@ -3,3 +3,12 @@ export type FormDataType = {
   email: string;
   password: string;
 };
+
+// export type Post = {
+//   _id: string;
+//   title: string;
+//   content: string;
+//   image: string;
+//   category: string;
+//   createdAt: Date;
+// };
