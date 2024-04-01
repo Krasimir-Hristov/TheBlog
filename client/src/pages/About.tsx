7ecb9d4830397855e7f8d7e3be60f08d3dab1097
krasi <krasimir.xristov@gmail.com>
2024-04-01 19:46:25 +0200

    complete about and project pages

--- client/src/pages/About.tsx
+++ client/src/pages/About.tsx
@@ -1,5 +1,42 @@
 const About = () => {
-  return <div>About</div>;
+  return (
+    <div className='min-h-screen flex items-center justify-center'>
+      <div className='max-w-5xl mx-auto p-3 text-center'>
+        <div>
+          <h1 className='text-3xl font-semibold text-center my-7'>
+            About Eleni's Bekery
+          </h1>
+          <div className='text-md text-gray-500 flex flex-col gap-6'>
+            <p>
+              Eleni's Bakery is a family-owned bakery that has been serving the
+              community for over 30 years. We specialize in custom cakes,
+              cookies, and other baked goods for all occasions. Our mission is
+              to provide our customers with delicious, high-quality baked goods
+              that are made
+            </p>
+
+            <p>
+              with love and care. We take pride in our work and strive to create
+              beautiful and tasty treats that will make your special occasion
+              even more memorable. Whether you're celebrating a birthday,
+              wedding, anniversary, or any other special event, we can create a
+              custom cake or dessert that will be the perfect centerpiece for
+              your celebration.
+            </p>
+
+            <p>
+              Our bakery is known for our attention to detail and our commitment
+              to using only the finest ingredients in all of our baked goods. We
+              offer a wide variety of flavors and designs to choose from, so you
+              can be sure that your cake or dessert will be exactly what you're
+              looking for. We also offer gluten-free and vegan options, so
+              everyone can enjoy our delicious treats.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default About;
--- client/src/pages/Projects.tsx
+++ client/src/pages/Projects.tsx
@@ -1,5 +1,20 @@
+import CallToAction from '../components/CallToAction';
+
 const Projects = () => {
-  return <div>Projects</div>;
+  return (
+    <div
+      className='min-h-screen max-w-2xl mx-auto flex justify-center items-center 
+    flex-col gap-6 p-3 text-center'
+    >
+      <h1 className='text-3xl font-semibold'> Projects</h1>
+      <p className='text-md text-gray-500'>
+        Eleni's Bakery is a family-owned bakery that has been serving the
+        community for over 30 years. We specialize in custom cakes, cookies, and
+      </p>
+
+      <CallToAction />
+    </div>
+  );
 };
 
 export default Projects;
