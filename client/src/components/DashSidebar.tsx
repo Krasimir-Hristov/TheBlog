bbe80450fcf07aabcca20eda872477399e96a65a
krasi <krasimir.xristov@gmail.com>
2024-04-01 15:58:10 +0200

    add dashboard overview to the admin dashboard

--- client/src/components/DashSidebar.tsx
+++ client/src/components/DashSidebar.tsx
@@ -5,6 +5,7 @@ import { Sidebar } from 'flowbite-react';
 import {
   HiAnnotation,
   HiArrowSmRight,
+  HiChartPie,
   HiDocumentText,
   HiUser,
 } from 'react-icons/hi';
@@ -50,6 +51,17 @@ const DashSidebar = () => {
     <Sidebar className='w-full md:w-56'>
       <Sidebar.Items>
         <Sidebar.ItemGroup className='flex flex-col gap-1'>
+          {currentUser && currentUser.isAdmin && (
+            <Link to='/dashboard?tab=dash'>
+              <Sidebar.Item
+                active={tab === 'dash' || !tab}
+                icon={HiChartPie}
+                as='div'
+              >
+                Dashboard
+              </Sidebar.Item>
+            </Link>
+          )}
           <Link to='/dashboard?tab=profile'>
             <Sidebar.Item
               active={tab === 'profile'}
